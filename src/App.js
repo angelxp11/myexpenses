48747c870167b00c5e3e9f93cecdbd5f77ba2d05
Jose Angel Bermúdez Choperena <jocheangel728@gmail.com>
2024-07-28 13:46:36 -0500

    apps.js arreglado con el login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
-import { getDoc, doc } from 'firebase/firestore'; // Importa las funciones necesarias
+import { getDoc, doc } from 'firebase/firestore';
 import { auth, db } from './firebase'; // Asegúrate de que la ruta es correcta
 import Login from './Login/login';
 import Home from './Webusuario/home';
-import AdminHome from './Webadmin/home'; // Importa otros componentes necesarios
+import AdminHome from './Webadmin/home'; // Asegúrate de que el nombre del archivo es correcto y está en la ruta correcta
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -16,9 +16,13 @@ function App() {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         // Verifica si el usuario es admin
-        const adminDoc = await getDoc(doc(db, 'admin', user.email));
-        setIsAdmin(adminDoc.exists());
-        setIsAuthenticated(true);
+        try {
+          const adminDoc = await getDoc(doc(db, 'admin', user.email));
+          setIsAdmin(adminDoc.exists());
+          setIsAuthenticated(true);
+        } catch (error) {
+          console.error('Error verificando el rol de admin:', error);
+        }
       } else {
         setIsAuthenticated(false);
       }
@@ -28,14 +32,26 @@ function App() {
     return () => unsubscribe();
   }, []);
 
-  if (loading) return <div></div>;
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
 
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to={isAdmin ? '/admin/home' : '/user/home'} />} />
-        <Route path="/user/home" element={isAuthenticated && !isAdmin ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/admin/home" element={isAuthenticated && isAdmin ? <AdminHome /> : <Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <Login /> : <Navigate to={isAdmin ? '/admin/home' : '/user/home'} />}
+        />
+        <Route
+          path="/user/home"
+          element={isAuthenticated && !isAdmin ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/admin/home"
+          element={isAuthenticated && isAdmin ? <AdminHome /> : <Navigate to="/login" />}
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
         {/* Define otras rutas aquí */}
       </Routes>
     </Router>
